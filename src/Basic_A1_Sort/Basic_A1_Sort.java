a469b7700ff4809d7c1cd5351b28f03ae045be32
Simon Blaha <s.blaha@live.at>
2019-05-29 09:53:09 +0200

    upload basic a1 with sort

---
+++ src/Basic_A1_Sort/Basic_A1_Sort.java
@@ -0,0 +1,17 @@
+package Basic_A1_Sort;
+
+import java.util.Arrays;
+
+public class Basic_A1_Sort {
+
+        public static int largest(int array[], int n) {
+            Arrays.sort(array);
+            return array[n - 1];
+
+
+        }
+
+
+    }
+
+
---
+++ src/Basic_A1_Sort/Main.java
@@ -0,0 +1,14 @@
+package Basic_A1_Sort;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int array[] = {1, 2, 3, 4 ,17 ,5, 6, 15, 16};
+
+        int n = array.length;
+
+        System.out.println(Basic_A1_Sort.largest(array, n));
+
+
+    }
+}
