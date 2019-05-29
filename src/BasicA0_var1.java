093604f1ca9ec5dbe6b9b22f175744e3aed03f53
judith <judithmarialang@gmail.com>
2019-05-29 09:36:29 +0200

    adds var1 for A0 exercise

---
+++ src/BasicA0_var1.java
@@ -0,0 +1,24 @@
+public class BasicA0_var1 {
+
+    static int arr[] = {10, 50, 70, 999, 23};
+
+    static int largest() {
+        int i;
+
+        int max = arr[0];
+
+        for (i = 1; i < arr.length; i++)
+            if (arr[i] > max)
+                max = arr[i];
+
+        return max;
+
+    }
+        public static void main (String[]args){
+
+
+            System.out.println("The largest number is " + largest());
+        }
+
+}
+
